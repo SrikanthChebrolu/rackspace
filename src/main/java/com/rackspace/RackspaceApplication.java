e22a7c7fc6c469eb42d683486454200261ecd54e
Srikanth Chebrolu <srikanth_chebrolu@gap.com>
2021-03-07 13:24:53 +0530

    [Srikanth Chebrolu] Initial Commit

---
+++ src/main/java/com/rackspace/RackspaceApplication.java
@@ -0,0 +1,13 @@
+package com.rackspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RackspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RackspaceApplication.class, args);
+	}
+
+}
